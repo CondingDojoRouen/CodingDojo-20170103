7c1ceba296487e086e692c50f27b436485aa4914
Pierrick Blons <Pierrick Blons>
2017-01-03 18:20:21 +0100

    Test renvoie 0 si liste vide

--- KataPotter/KataPotterTests/KataPotterTests.cs
+++ KataPotter/KataPotterTests/KataPotterTests.cs
@@ -1,11 +1,20 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingDojo;
 
-namespace KataPotterTests
+namespace CodingDojo
 {
     [TestClass]
     public class KataPotterTests
     {
-
+        [TestMethod]
+        public void Returns0IfNoBooks()
+        {
+            //Arrange
+            //Act
+            var result = KataPotter.GetPrice(new int[] { });
+            //Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
