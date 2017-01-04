193d20468a767b954e702eddcada1d054d2922b6
Pierrick Blons <Pierrick Blons>
2017-01-04 09:36:01 +0100

    Will fix tree later but forgot to version the source file :)

---
+++ KataPotter/KataPotter/KataPotter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo
+{
+    public static class KataPotter
+    {
+        public static float GetPrice(int[] books)
+        {
+            if (books.Any(b => b > 5 || b < 1))
+                throw new ArgumentException();
+
+            if (books.Length == 0)
+                return 0;
+
+            if (books.All(b => b == books.First()))
+                return books.Length * 8;
+
+            int bookFirst = books.FirstOrDefault();
+            if (books.Any(b => b != bookFirst))
+                return books.Length * 8 * GetCoef(books.Length);
+
+            return 8;
+        }
+
+        public static float GetCoef(int booksNumber)
+        {
+            switch (booksNumber)
+            {
+                case 2: return 0.95f;
+                case 3: return 0.90f;
+                    //case 4: return 0.80f;
+                    //case 5: return 0.75f;
+            }
+            return 1;
+        }
+    }
+}
